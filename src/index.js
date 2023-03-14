9476aa2da93709e7519b2154411d26587c452637
MontserratPoblano <christianpoblano@gmail.com>
2023-03-14 09:38:00 -0600

    Se realizaron los primeros cambios en HTML,JS Y CSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import cipher from './cipher.js';
 
+let buttonCipher=document.getElementById("button-cipher");
+
+buttonCipher.addEventListener("click",()=>{
+    let initialMessage = document.getElementById("initial-message").value;
+    let keyCipher=document.getElementById("key-cipher").value;
+    let divide=initialMessage.split("");
+    let newNumbers={};
+    for(let i=0;i<divide.length;i++){
+    newNumbers[i]=divide[i].charCodeAt(); 
+    } 
+    return newNumbers;
+    
+
+});
+
+
 console.log(cipher);
